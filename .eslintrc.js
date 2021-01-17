2a537486e3a1e6c25b26102386fc93913291f6e0
Mr Jin <haosheng0211@yahoo.com.tw>
2021-01-17 16:18:02 +0800

    配置VueJS

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended"
+  ],
+  plugins: [
+    "vue"
+  ],
+  rules: {}
+};
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+  entry: "./views/main.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "js/app.js"
+  },
+  resolve: {
+    extensions: [".js", ".vue"],
+    alias: {
+      "vue": "vue/dist/vue.js"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules)/,
+        use: [
+          "babel-loader",
+          "eslint-loader"
+        ]
+      },
+      {
+        test: /\.(vue)$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            esModule: false,
+            limit: 1000, //bytes
+            name: "[hash:7].[ext]",
+            outputPath: "image"
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 1000, //bytes
+            name: "[hash:7].[ext]",
+            outputPath: "fonts"
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          "vue-style-loader",
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "./"
+            }
+          },
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/app.css"
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["js/*", "css/*", "fonts/*", "images/*"]
+    }),
+    new VueLoaderPlugin()
+  ]
+};
