e14767d45e61afaaf8d7d9cf8127ccd95eb8552d
Mr Jin <haosheng0211@yahoo.com.tw>
2021-01-17 18:28:04 +0800

    修改公共路徑位置

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: "./"
+              publicPath: "/"
             }
           },
           "css-loader",
