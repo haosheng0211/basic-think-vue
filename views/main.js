8e389a72c467e848ac962c2a08e2a85ef1747d61
Mr Jin <haosheng0211@yahoo.com.tw>
2021-01-17 18:32:39 +0800

    基礎配置

--- views/main.js
+++ views/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
+import VueMeta from "vue-meta";
+import app from "@/app";
 import router from "@/router";
 
+Vue.use(VueMeta);
 new Vue({
-  router
+  router,
+  render: h => h(app)
 }).$mount("#app");
